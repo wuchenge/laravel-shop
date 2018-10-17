16ecce53c7e394682d4b95a7f077708291db7b9f
无尘阁 <ligui0506@126.com>
2018-10-17 13:29:11 +0800

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://www.wuchenge.com" target="_blank">wuchenge</a> 编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:ligui0506@126.com">联系我们</a></p>
+    </div>
+</footer>
