26776865cbdf1dbbab211336e15011d5720681b9
无尘阁 <ligui0506@126.com>
2018-10-17 18:12:15 +0800

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::group([
 ], function () {
     Route::redirect('/', '/products')->name('root');
     Route::get('products', 'ProductsController@index')->name('products.index');
+    Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 });
 
 Auth::routes();
