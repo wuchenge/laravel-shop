5f1e6e4e4651e133680096f0e83b184494f583de
无尘阁 <ligui0506@126.com>
2018-10-17 15:06:58 +0800

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,9 @@ Route::group([
     'namespace' => 'Home'
 ], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
 
     Route::group(['middleware' => 'email_verified'], function () {
-        Route::get('/test', function () {
-            return 'Your email is verified';
-        });
     });
 });
