0fd070b349d119931f9f6f28e992a3bdb91ffb16
无尘阁 <ligui0506@126.com>
2018-10-18 14:37:36 +0800

    订单支付

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,13 @@ Route::group([
         Route::post('orders', 'OrdersController@store')->name('orders.store');
         Route::get('orders', 'OrdersController@index')->name('orders.index');
         Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+
+        Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+        Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     });
 });
+
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 
 
 Route::group([
