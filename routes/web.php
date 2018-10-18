f2b76fbb489dbbecd1f0b7b612ff472177767413
无尘阁 <ligui0506@126.com>
2018-10-18 11:27:28 +0800

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::group([
         Route::post('cart', 'CartController@add')->name('cart.add');
         Route::get('cart', 'CartController@index')->name('cart.index');
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+        Route::post('orders', 'OrdersController@store')->name('orders.store');
     });
 });
 
