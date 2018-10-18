1b0995d319c86b3261687cf9a6a4bbdd7a18b7cb
无尘阁 <ligui0506@126.com>
2018-10-18 16:39:50 +0800

    确认收货

--- app/Http/Controllers/Home/OrdersController.php
+++ app/Http/Controllers/Home/OrdersController.php
@@ -11,6 +11,12 @@ use App\Http\Requests\OrderRequest;
 
 class OrdersController extends Controller
 {
+    /**
+     * 创建订单
+     * @param  OrderRequest $request      [description]
+     * @param  OrderService $orderService [description]
+     * @return [type]                     [description]
+     */
     public function store(OrderRequest $request, OrderService $orderService)
     {
         $user    = $request->user();
@@ -19,6 +25,11 @@ class OrdersController extends Controller
         return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
     }
 
+    /**
+     * 订单列表
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
     public function index(Request $request)
     {
         $orders = Order::query()
@@ -31,9 +42,38 @@ class OrdersController extends Controller
         return view('orders.index', ['orders' => $orders]);
     }
 
+    /**
+     * 订单详情
+     * @param  Order   $order   [description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
     public function show(Order $order, Request $request)
     {
         $this->authorize('own', $order);
         return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
+
+    /**
+     * 确认收货
+     * @param  Order   $order   [description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function received(Order $order, Request $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+
+        // 判断订单的发货状态是否为已发货
+        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
+            throw new InvalidRequestException('发货状态不正确');
+        }
+
+        // 更新发货状态为已收到
+        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
+
+        // 返回原页面
+        return $order;
     }
 }
