91e63c720690557669b42cdee74fa70b9d763dac
无尘阁 <ligui0506@126.com>
2018-10-18 13:50:22 +0800

    引入支付库

---
+++ config/pay.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @Author: wuchenge
+ * @Date: 2018-10-18 13:47:50
+ */
+
+return [
+    'alipay' => [
+        'app_id'         => '',
+        'ali_public_key' => '',
+        'private_key'    => '',
+        'log'            => [
+            'file' => storage_path('logs/alipay.log'),
+        ],
+    ],
+
+    'wechat' => [
+        'app_id'      => '',
+        'mch_id'      => '',
+        'key'         => '',
+        'cert_client' => '',
+        'cert_key'    => '',
+        'log'         => [
+            'file' => storage_path('logs/wechat_pay.log'),
+        ],
+    ],
+];
